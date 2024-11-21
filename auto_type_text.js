d72895f9eefbffe7ee259ca8d9089ff5b337f676
AbdullahRFA <shakibrybmn@gmail.com>
2024-11-22 01:20:51 +0600

    Added header and auto type effect using js

---
+++ auto_type_text.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#auto_type', {
+    strings: ['Auto Rice Mill Management '],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true // Set this to true for infinite looping
+});
